36e62d0fe0d3fd9ab3ad1ed2b444bbcae6ac8854
Athysan <athayahsn@gmail.com>
2026-07-22 22:43:14 +0700

    Cleanup backend-express: remove unused test and setup scripts (create_bucket.js, test_db.js, test_insert.js)

--- backend-express/test_db.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-supabase.from('products').select('*').limit(1).then(res => {
-  if (res.data && res.data.length > 0) console.log("COLUMNS:", Object.keys(res.data[0]));
-  else console.log("Empty or Error:", res.error);
-});
